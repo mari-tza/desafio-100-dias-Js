75c9ce99666a42664f0d87afb00a709242ec0728
mari-tza <maaritzacastro@gmail.com>
2023-04-17 22:57:28 -0300

    dia 7 e 8

---
+++ dia-7-profile-card-animado/script.js
@@ -0,0 +1,10 @@
+(function() {
+    $('.btn').click(function() {
+      $(this).toggleClass('active');
+      return $('.box').toggleClass('open');
+    });
+  
+  }).call(this);
+  
+  //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFBQSxDQUFBLENBQUUsTUFBRixDQUFTLENBQUMsS0FBVixDQUFnQixRQUFBLENBQUEsQ0FBQTtJQUNmLENBQUEsQ0FBRSxJQUFGLENBQU8sQ0FBQyxXQUFSLENBQW9CLFFBQXBCO1dBQ0EsQ0FBQSxDQUFFLE1BQUYsQ0FBUyxDQUFDLFdBQVYsQ0FBc0IsTUFBdEI7RUFGZSxDQUFoQjtBQUFBIiwic291cmNlc0NvbnRlbnQiOlsiJCgnLmJ0bicpLmNsaWNrIC0+XG5cdCQodGhpcykudG9nZ2xlQ2xhc3MgJ2FjdGl2ZSdcblx0JCgnLmJveCcpLnRvZ2dsZUNsYXNzICdvcGVuJyJdfQ==
+  //# sourceURL=coffeescript
